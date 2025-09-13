549837fec379017ba2dc174c373d32713f11bc5c
Vamsee1317 <vamsee.achieversit@gmail.com>
2025-09-13 13:29:56 +0530

    Forms | Input

---
+++ html/forms/theory.js
@@ -0,0 +1,13 @@
+// Form :
+
+// Forms are used to collect user input.
+// Form can contain various input elements like text fields, checkboxes, radio buttons, submit buttons, etc.
+
+// Example of a simple form:
+
+
+// Authentication :
+// Authorization :
+// Validation :
+// Verification :
+
